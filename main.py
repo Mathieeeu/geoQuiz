dc78172ed79b9cbd47069f71ceb21f77458f4f5c
LéandroV <67877899+MagicArrow26@users.noreply.github.com>
2021-01-29 09:54:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+import tkinter.font as tkFont
 from tkinter import *
 
 
@@ -7,31 +9,57 @@ def page1():
     menu.place(x=0, y=0)
     
     bouton_jouer = StringVar()
-    bouton_jouer=Button(menu, text='Jouer',command=page2)
+    bouton_jouer=Button(menu, text='Jouer',command=page2, font=police)
     bouton_jouer.place(x=(int(longueur)/2)-100,y=300,width=200, height=50)
 
     bouton_quit = StringVar()
-    bouton_quit=Button(menu, text='Quitter',command=quitter)
+    bouton_quit=Button(menu, text='Quitter',command=quitter, font=police)
     bouton_quit.place(x=(int(longueur)/2)-100,y=500,width=200, height=50)
     
     print('p1 appelé')
 
 def page2():
     global jeu
+    global textBoxEntree1
+    global textBoxEntree2
     reset()
     jeu.place(x=0, y=0)
 
     bouton_retour = StringVar()
-    bouton_retour=Button(jeu, text='Retour', command=page1)
+    bouton_retour=Button(jeu, text='Retour', command=page1, font=police)
     bouton_retour.place(x=int(longueur)-210,y=int(largeur)-60,width=200, height=50)
 
     print('p2 appelé')
 
-    textBoxEntree = Entry(jeu, textvariable=var_entree, width=40)
-    textBoxEntree.place(x=410,y=635,width=200, height=70)
+    textBoxEntree1 = Entry(jeu, textvariable=var_entree1, width=40, font=police)
+    textBoxEntree1.place(x=600,y=100,width=200, height=70)
 
-    label_q1 = Label(jeu, text=str(question), police=...)
-    label_q1.place(x=200,y=235,width=200, height=70)
+    label_q1 = Label(jeu, text=str(question1), font=police)
+    label_q1.place(x=200,y=100,width=400, height=70)
+
+    textBoxEntree2 = Entry(jeu, textvariable=var_entree2, width=40, font=police)
+    textBoxEntree2.place(x=600,y=200,width=200, height=70)
+
+    label_q2 = Label(jeu, text=str(question2), font=police)
+    label_q2.place(x=200,y=200,width=400, height=70)
+
+    textBoxEntree3 = Entry(jeu, textvariable=var_entree3, width=40, font=police)
+    textBoxEntree3.place(x=600,y=300,width=200, height=70)
+
+    label_q3 = Label(jeu, text=str(question3), font=police)
+    label_q3.place(x=200,y=300,width=400, height=70)
+    
+    textBoxEntree4 = Entry(jeu, textvariable=var_entree4, width=40, font=police)
+    textBoxEntree4.place(x=600,y=400,width=200, height=70)
+    
+    label_q4 = Label(jeu, text=str(question4), font=police)
+    label_q4.place(x=200,y=400,width=400, height=70)
+
+    textBoxEntree5 = Entry(jeu, textvariable=var_entree5, width=40, font=police)
+    textBoxEntree5.place(x=600,y=500,width=200, height=70)
+
+    label_q5 = Label(jeu, text=str(question5), font=police)
+    label_q5.place(x=200,y=500,width=400, height=70)
 
 
 
@@ -48,12 +76,18 @@ def quitter():   #fermer la fenetre
     fenetre.destroy()
 
 def callback(key):
-    recherche()
-
-def recherche():
-    print(str(var_entree.get()))
+    verif()
 
 
+def verif():
+    global textBoxEntree1
+    global textBoxEntree2
+    if str(var_entree1.get()) == 'Paris':
+        print('gg')
+        label_r1 = Label(jeu, textvariable=var_entree1, font=police)
+        label_r1.place(x=600,y=100,width=200, height=70)
+        textBoxEntree1.configure(state="disable")
+        textBoxEntree2.configure(state="normal")
 
 longueur = '1440'
 largeur = '810'
@@ -61,16 +95,32 @@ largeur = '810'
 fenetre = Tk()
 fenetre.geometry(longueur+'x'+largeur)
 fenetre.title('GéoQuiz')
-Police=fenetre.font.Font(family="Calibri",size=120)
+police=tkFont.Font(family="Lobster",size=18)
 
 menu = Canvas(fenetre, width=longueur, height=largeur)
 jeu = Canvas(fenetre, width=longueur, height=largeur)
 
 fenetre.bind('<Return>', callback)
 
-question='Quel est la capitale de la France ?'
 
-var_entree = StringVar()
+
+question1 = StringVar()
+question2 = StringVar()
+question3 = StringVar()
+question4 = StringVar()
+question5 = StringVar()
+var_entree1 = StringVar()
+var_entree2 = StringVar()
+var_entree3 = StringVar()
+var_entree4 = StringVar()
+var_entree5 = StringVar()
+
+question1='Quel est la capitale de la France ?'
+question2='Quel est la capitale de la France ?'
+question3='Quel est la capitale de la France ?'
+question4='Quel est la capitale de la France ?'
+question5='Quel est la capitale de la France ?'
 
 page1()
 menu.mainloop()
+
