bb7d0ea722c0a408e23e272b7a7e1d99444df48e
diogeek <dionys.colson@laposte.net>
2021-02-26 08:15:37 +0100

    rastart

---
+++ test-des-questions mieux.py
@@ -0,0 +1,53 @@
+import sqlite3
+from random import *
+question=1
+
+"""Fonction de connexion permettant de se connecter à la base
+"""
+def connexion():
+    try:
+        #connexion à la bdd
+        sqliteConnection = sqlite3.connect('pays.db')
+        return sqliteConnection
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite : ", error)
+
+"""<summary>Fonction de connexion à la bdd</summary>
+"""
+def deconnexion(sqliteConnection):
+   if (sqliteConnection):
+       #fermeture de la co
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
+
+
+def AffichezDB():
+    sqliteConnection = connexion()
+    cursor = sqliteConnection.cursor()
+    #ecriture de la requéte, on récupére le contenu de la listeDeroulante avec la fonction .get()
+    #sqlite_select_Query = ##########################################################################
+    #execution de la requéte
+    cursor.execute(sqlite_select_Query)
+    #on place tout les enregistrements dans une variable record
+    record = cursor.fetchall()
+    return record
+
+def random():
+    chiffre=randint(1,199)
+    sqliteConnection = connexion()
+    cursor = sqliteConnection.cursor()
+    #ecriture de la requéte, on récupére le contenu de la listeDeroulante avec la fonction .get()
+    sqlite_select_Query = ("select nom from pays where idPays="+str(chiffre))
+    #execution de la requéte
+    cursor.execute(sqlite_select_Query)
+    #on place tout les enregistrements dans une variable record
+    record = cursor.fetchall()
+    record= record[0][0]
+    return record
+
+listealpha=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+#---------------------------------------------------------------------------------------------
+
+while 1:
+    print(random()+"\n\n")
