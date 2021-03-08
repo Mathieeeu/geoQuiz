669bec6271996e4b7ff081ba744c0cf40a3bee2a
diogeek <dionys.colson@laposte.net>
2021-03-08 17:43:29 +0100

    PUITAIN CA MARCHE
    MDRTRRR

--- test-des-questions mieux.py
+++ test-des-questions mieux.py
@@ -62,7 +62,25 @@ def AffichezDB(nom):
     pays1 = pays(record[0][1],record[0][2],record[0][3],record[0][4],record[0][5])
     return pays1
 
-
+def enigme(attribut):
+    if attribut=="superficie":
+        supermin,supermax = question(pays1,"superficie")
+        if supermax == 999999999 :
+            return("Ce pays fait plus de " + str(supermin) + " km² !")
+        else : return("Ce pays fait entre " + str(supermin) + " et " + str(supermax) + " km² !")
+    if attribut == "population" :
+        popmin,popmax = question(pays1,"population")
+        if popmax == 999999999 :
+            return("Ce pays compte plus de " + str(popmin) + " habitants !")
+        else : return("Ce pays compte entre " + str(popmin) + " et " + str(popmax) + " habitants !")
+    if attribut == "initiale_nom" :
+        initiale_pays=question(pays1,"initiale_nom")
+        return("Ce pays commence par un "+str(initiale_pays))
+    if attribut == "initiale_capitale" :
+        initiale_cap=question(pays1,"initiale_capitale")
+        return("La capitale de ce pays commence par un "+str(initiale_cap))
+        
+        #print(AffichezDB(nompays,listeQ[randint(0,len(listeQ)-1)]))
 
 def question(pays,attribut):
     i1,i2=0,0
@@ -84,14 +102,14 @@ def question(pays,attribut):
                 i1=9999999999
             if liste[i] <= pays.population and pays.population <= i1 :
                 return(liste[i],i1)
-    if attribut == "initiale_pays":
+    if attribut == "initiale_nom":
         mot=pays.nom[0]
         mot = mot.lower()
         mot = mot.replace("é","e")
         mot = mot.replace("î","i")
         mot=mot.upper()
         return mot
-    if attribut == "initiale_cap":
+    if attribut == "initiale_capitale":
         mot=pays.capitale[0]
         mot = mot.lower()
         mot = mot.replace("é","e")
@@ -99,32 +117,51 @@ def question(pays,attribut):
         mot=mot.upper()
         return mot
 
-    
-listealpha=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+def calcul(valeur,attribut):
+    if attribut == "initiale_nom" or attribut == "initiale_capitale":   # POUR LES LETTRE
+        attribut=attribut.replace("initiale_","")
+        condition = (attribut + " like \'"+valeur+"%'")
+    else :                                                                                          #POUR LES CHIFFRES MIAM
+        condition = (attribut + " > " + str(valeur[0]) + " and " + attribut + " < " + str(valeur[1]))
+    sqliteConnection = connexion()
+    cursor = sqliteConnection.cursor()
+    #ecriture de la requéte, on récupére le contenu de la listeDeroulante avec la fonction .get()
+    sqlite_select_Query = ("select nom from pays where "+ condition)
+    #execution de la requéte
+    cursor.execute(sqlite_select_Query)
+    #on place tout les enregistrements dans une variable record
+    record = cursor.fetchall()
+    print(record)
+    return(len(record))
 
-#---------------------------------------------------------------------------------------------
+
+# listealpha=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 tirage=True
 nompays=random()
-listeQ=["superficie"]
+listeQ=["superficie","initiale_nom","initiale_capitale","population"]
 while 1:
     if tirage==True:
         print("______________________________________________________\n"+str(nompays))
         pays1=AffichezDB(nompays)
         tirage=False
         print(pays1.affichageinfos())
-        supermin,supermax = question(pays1,"superficie")
-        if supermax == 999999999 :
-            print("Ce pays fait plus de " + str(supermin) + " km² !")
-        else : print("Ce pays fait entre " + str(supermin) + " et " + str(supermax) + " km² !")
-        popmin,popmax = question(pays1,"population")
-        if popmax == 999999999 :
-            print("Ce pays compte plus de " + str(popmin) + " habitants !")
-        else : print("Ce pays compte entre " + str(popmin) + " et " + str(popmax) + " habitants !")
-        initiale_pays=question(pays1,"initiale_pays")
-        print("Ce pays commence par un "+str(initiale_pays))
-        initiale_cap=question(pays1,"initiale_cap")
-        print("La capitale de ce pays commence par un "+str(initiale_cap))
-        #print(AffichezDB(nompays,listeQ[randint(0,len(listeQ)-1)]))
-    if input("")=="":
-        nompays=random()
-        tirage=True
+
+
+        print("\n#############################################")
+        attribut = listeQ[randint(0,len(listeQ)-1)]
+        valeur = question(pays1,attribut)
+        issues = calcul(valeur,attribut)
+        print("\n\n\nIl y a un cetain combre de possiblites qui est egal au chiffr esuviant ------------>        ",issues)
+        
+        print("\n\nla condititon était    --------->",enigme(attribut))
+        
+        if issues < 20 :
+            tirage = True
+        else : break
+    #if input("")=="":
+      #  nompays=random()
+        #tirage=True
+    
+        
